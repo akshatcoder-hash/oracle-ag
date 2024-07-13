6ebf2f3756667638e85c2417b240524d1c99ee3b
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-13 14:25:05 +0530

    add types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module 'crypto-browserify';
+declare module 'stream-browserify';
+declare module 'assert';
+declare module 'buffer';
+declare module 'util';
+declare module 'readable-stream';
+declare module 'events';
