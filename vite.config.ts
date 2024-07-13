1e4cb9d6e14fba8afe259ea2d6cb59903c33a590
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-13 14:17:21 +0530

    vite and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      buffer: 'buffer',
+      util: 'util',
+      'readable-stream': 'readable-stream',
+      events: 'events',
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  optimizeDeps: {
+    include: ['buffer', 'crypto-browserify', 'stream-browserify', 'assert', 'util', 'readable-stream', 'events'],
+    esbuildOptions: {
+      target: 'esnext',
+      supported: { bigint: true },
+    },
+  },
 })
